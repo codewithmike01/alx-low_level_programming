356225b9dfb846a51e35d2cd9c90f5f2aaee051d
ginohmk <michotall95@gmail.com>
2022-09-09 11:02:44 -0700

    all

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit, innerDigit;
+for (digit = '0'; digit < '98'; digit++)
+{
+for (innerDigit = digit + 1; innerDigit <= '99'; innerDigit++)
+{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+if (innerDigit == '99' && digit == '98')
+{
+putchar(digit);
+putchar(innerDigit);
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}

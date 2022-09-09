536b4fe571133ffd07f41dd50ab3cb856157ea98
ginohmk <michotall95@gmail.com>
2022-09-09 05:09:13 -0700

    Add new line to printf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n == 0)
 {
-printf("%d is zero",n);
+printf("%d is zero\n",n);
 }
 else if(n > 0)
 {
@@ -21,7 +21,7 @@ printf("%d is positive\n",n);
 }
 else
 {
-printf("%d is negative", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }

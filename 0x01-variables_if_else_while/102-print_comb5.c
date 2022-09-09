bcde79873f6469ecbd00355c169c2fa0d083b1a1
ginohmk <michotall95@gmail.com>
2022-09-09 11:06:11 -0700

    all

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 int digit, innerDigit;
-for (digit = '0'; digit < '98'; digit++)
+for (digit = 0; digit <= 98; digit++)
 {
-for (innerDigit = digit + 1; innerDigit <= '99'; innerDigit++)
+for (innerDigit = digit + 1; innerDigit <= 99; innerDigit++)
 {
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
 	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
-if (innerDigit == '99' && digit == '98')
+if (innerDigit == 99 && digit == 98)
 {
 putchar(digit);
 putchar(innerDigit);

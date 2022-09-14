06fd0b4431e17794244637b5ab2d35038e9c0dec
ginohmk <michotall95@gmail.com>
2022-09-14 01:18:07 -0700

    all

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,16 +5,14 @@
  * main - check the code 
  * Return: Always 0.
  */
-int main(void)
+int _abs(int r)
 {
-int r;
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+if (r >= 0)
+{
+return (r);
+}
+else
+{
+return (r * -1);
+}
 }

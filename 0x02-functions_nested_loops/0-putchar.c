ea59f6a21aec68585b9e879d4817635b7f72989a
ginohmk <michotall95@gmail.com>
2022-09-14 03:34:40 -0700

    all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,14 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Description: It prints the word _putchar followed by a new line
+ * Return: 0.
  */
 int main(void)
 {
-char word[9] = "_putchar";
+char word[8] = "_putchar";
 int i;
-for (i = 0; i < 9; i++)
+for (i = 0; i < 8; i++)
 {
 _putchar(word[i]);
 }

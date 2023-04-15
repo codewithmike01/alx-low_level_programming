c8999856c87bf41e4ad5b240c2f50b7a104060f5
Kanu Mike Chibundu <michotall95@gmail.com>
2023-04-15 17:44:17 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * get_bit - A function that gets a bit at index
  * @n: The number to get bit from
  * @index: The index where the bit get at
- * Return: The value of the bit or -1 if an error occured
+ * Return: The value of the bit or -1 an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {

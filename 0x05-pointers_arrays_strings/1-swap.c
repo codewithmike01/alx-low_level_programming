5d58faf07c376c850b8685753be70e7350be3064
Kanu Mike Chibundu <michotall95@gmail.com>
2023-05-08 19:58:28 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 
 /**
  * swap_int -  a function that swaps the values of two integers.
- * @a: an inout integer pointer
+ * @a: an input integer pointer
  * @b: an input integer pointer
  * Return: nothing at all
  */
